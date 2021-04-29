f06aa9c0927d44fb83bb31f740a31f043ae62be4
mhanifmustakim <mhanifmustakim@gmail.com>
2021-04-29 14:30:45 +0800

    Written the logic for First to 5, w/o error detection

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ function computerPlay() {
     return cpInput;
 }
 
-function playGame(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
+    playerSelection.toLowerCase();
     const winScript = `You Win! ${playerSelection} beats ${computerSelection}!`;
     const loseScript = `You Lose! ${computerSelection} beats ${playerSelection}!`;
 
@@ -21,24 +22,49 @@ function playGame(playerSelection, computerSelection) {
         return "It's a tie!"
     } else if (playerSelection === "rock") {
         if (computerSelection === "scissors") {
+            pScore++;
             return winScript;
         } else {
+            cScore++;
             return loseScript;
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
+            pScore++;
             return winScript;
         } else {
+            cScore++;
             return loseScript;
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "paper") {
+            pScore++;
             return winScript;
         } else {
+            cScore++;
             return loseScript;
         }
     } else {
-        return null;
+        return "You entered an invalid key";
     }
 }
 
+function game() {
+    let input = prompt("Select rock,paper or scissors");
+    console.log("***********************");
+    console.log(playRound(input, computerPlay()));
+    console.log(`player score is : ${pScore}, computer score is : ${cScore}`)
+}
+
+let cScore = 0, pScore = 0;
+let isGameOver = false;
+while (true) {
+    game();
+    if (pScore === 5) {
+        console.log("Player Wins!")
+        break;
+    } else if (cScore === 5) {
+        console.log("Computer Wins!");
+        break;
+    }
+}
